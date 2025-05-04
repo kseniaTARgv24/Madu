2749f9498a9c9957ffd808ccca4061de82e3aee1
kseniaTARgv24 <ksenia.pleshakova2001@gmail.com>
2025-05-04 18:19:40 +0300

    3 lesson completed

--- Madu/Program.cs
+++ Madu/Program.cs
@@ -10,10 +10,8 @@ namespace Madu
     {
         static void Main(string[] args) {
 
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1,2,'x');
+
             p1.Draw();
              
         }
