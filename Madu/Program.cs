dfe7aeccf3d827f3d3cdbde6e46584fccf362233
kseniaTARgv24 <ksenia.pleshakova2001@gmail.com>
2025-05-04 17:54:53 +0300

    2 lesson is completed

--- Madu/Program.cs
+++ Madu/Program.cs
@@ -10,27 +10,12 @@ namespace Madu
     {
         static void Main(string[] args) {
 
-            int x1 = 1;
-            int y1 = 2;
-            char sym1 = '*';
-            
-            Draw(x1, y1, sym1);
-
-            Console.ReadLine();
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
-
-            Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char sym) 
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+             
         }
 
     }
